fd1a8a3bcab19b97db26dd12b552578da1add742
Lucas Benvenuti <lucasbenvenuti2010@gmail.com>
2017-02-16 15:05:21 -0200

    Capitulo04-aula01 [v2]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 import { MenuTestPage } from '../pages/menu-test/menu-test';
-
+import { GeneratedTestPage } from '../pages/generated-test/generated-test';
 
 @Component({
   templateUrl: 'app.html'
@@ -24,7 +24,8 @@ export class MyApp {
 
     this.pages = [
       {component: HomePage, title: 'Home', icon: 'home'},
-      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'}
+      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'},
+      {component: GeneratedTestPage, title: 'Generated Page', icon: 'home'}
 
     ]
 
